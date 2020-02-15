1fc944f210b36446925297762af822982fba9a3d
Amar Jasarbasic <amarjasarbasic@gmail.com>
2020-02-15 02:28:26 -0800

    added randomizer for words

--- morning-app/src/app/word-cloud/word-cloud.component.ts
+++ morning-app/src/app/word-cloud/word-cloud.component.ts
@@ -12,9 +12,27 @@ export class WordCloudComponent implements OnInit {
 
   ngOnInit() {
 
+    let words = this.randomNumbers(25, 1000);
+
     let circleManager = new CircleManager('container');
     circleManager.add("Hello there!Hello there!Hello there!Hello there!Hello there!Hello there!Hello there!Hello there!");
     circleManager.add("Hello there!");
+
   }
+
+  randomNumbers(n, max) {
+    var nums = [],
+      randomNumber;
+
+    for (var l = 0; l < n; l++) {
+      do {
+        randomNumber = Math.floor(Math.random() * max);
+      } while (nums.includes(randomNumber));
+      nums.push(randomNumber);
+    }
+
+    return nums;
+  }
+
 
 }
