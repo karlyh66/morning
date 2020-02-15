c1f364b858ec17221889ed6419dd89427dbbb2f6
Amar Jasarbasic <amarjasarbasic@gmail.com>
2020-02-15 01:00:38 -0800

    added bubble stuff

--- morning-app/src/app/app.module.ts
+++ morning-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
+import { WordCloudComponent } from './word-cloud/word-cloud.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleBarComponent
+    TitleBarComponent,
+    WordCloudComponent
   ],
   imports: [
     BrowserModule,
