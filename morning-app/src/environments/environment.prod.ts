8e75ae9576f65e87a2ef5100f1cac204d1b59935
Deblob12 <jeffrey_chiu@college.harvard.edu>
2020-02-15 10:21:55 -0800

    Firebase

--- morning-app/src/environments/environment.prod.ts
+++ morning-app/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCb1t8S5Z7kVibR1kFDNlpQxiOGyeZLofo",
+    authDomain: "wakeup-9fbd3.firebaseapp.com",
+    databaseURL: "https://wakeup-9fbd3.firebaseio.com",
+    projectId: "wakeup-9fbd3",
+    storageBucket: "wakeup-9fbd3.appspot.com",
+    messagingSenderId: "1026287447856",
+    appId: "1:1026287447856:web:872340677e8830d0a322f5",
+    measurementId: "G-S8E4ZRPHYX"
+  }
 };
