7bf1248d1fba24a340ca6adb31a6ada58c10dc2e
Amar Jasarbasic <amarjasarbasic@gmail.com>
2020-02-14 23:42:52 -0800

    added more to boiler plate

--- morning-app/src/app/app.module.ts
+++ morning-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleBarComponent
   ],
   imports: [
     BrowserModule,
