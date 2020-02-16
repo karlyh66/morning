004c079e255d5cb3e855deb9074f53068aab7ad8
Amar Jasarbasic <amarjasarbasic@gmail.com>
2020-02-15 17:45:03 -0800

    implemented auth with firebase

--- morning-app/src/app/app-routing.module.ts
+++ morning-app/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from  './signin/signin.component';
+import { WordCloudComponent } from './word-cloud/word-cloud.component';
+import { AuthGuard } from './services/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: SigninComponent },
+  { path: 'home', component: WordCloudComponent,  canActivate: [AuthGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- morning-app/src/app/app.module.ts
+++ morning-app/src/app/app.module.ts
@@ -4,27 +4,38 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AuthGuard } from './services/auth.guard';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
 import { WordCloudComponent } from './word-cloud/word-cloud.component';
-import { NewUserComponent } from './new-user/new-user.component';
+import { SigninComponent } from './signin/signin.component';
+import { SuiModule } from 'ng2-semantic-ui';
+import { UserService } from './services/user.service';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleBarComponent,
     WordCloudComponent,
-    NewUserComponent
+    SigninComponent
   ],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
- 	  AngularFirestoreModule,
+    AngularFirestoreModule,
+    SuiModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
